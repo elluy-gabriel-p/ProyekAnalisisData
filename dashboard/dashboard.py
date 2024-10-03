c8a15daaa81f13905dc1137591a8f52b35c1c5bb
01OUTCAST10 <elluygabrielpanambe@gmail.com>
2024-10-03 11:01:47 +0700

    Refactor file paths and remove deprecated option in dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -8,7 +8,7 @@ from babel.numbers import format_currency
 from func import DataAnalyzer, BrazilMapPlotter
 
 sns.set(style='darkgrid')
-st.set_option('deprecation.showPyplotGlobalUse', False)
+# st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Dataset
 datetime_cols = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
